e861736ff098872b898c4fc2587ead21c1cda3f5
syairsiska <122198200+syairsiska@users.noreply.github.com>
2023-01-08 18:55:43 +0800

    Update post.js

--- post.js
+++ post.js
@@ -4,8 +4,9 @@ _Hasync.push(['Histats.fasi', '1']);
 _Hasync.push(['Histats.track_hits', '']);
 (function() {
 var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-hs.src = ('//raw.githack.com/syairsiska/jitu/main/js15_as.js');
+hs.src = ('//rawcdn.githack.com/syairsiska/jitu/a71067379fa4b69073ce1c97a0b15cb255487343/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
 })();
+
 
 
