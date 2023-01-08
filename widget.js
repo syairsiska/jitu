3d089d5c90f65f95d8cff51b6a4ac0dc95ba17bc
syairsiska <122198200+syairsiska@users.noreply.github.com>
2023-01-08 15:45:12 +0800

    Update widget.js

--- widget.js
+++ widget.js
@@ -4,7 +4,8 @@ _Hasync.push(['Histats.fasi', '1']);
 _Hasync.push(['Histats.track_hits', '']);
 (function() {
 var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-hs.src = ('//cdn.statically.io/gh/syairsiska/jitu/main/js15_as.js');
+hs.src = ('//cdn.jsdelivr.net/gh/syairsiska/jitu@main/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
 })();
+
 
